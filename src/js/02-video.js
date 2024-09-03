e77aac2cc6c41c7198b399685e7ec0bc50a0e6c5
Jigsaw28 <levenko7@gmail.com>
2024-09-03 21:24:09 +0300

    done task

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,22 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe#vimeo-player');
+const player = new Player(iframe);
+
+player.on('timeupdate', throttle(onPlayer, 1000));
+
+function onPlayer(data) {
+  localStorage.setItem('videoplayer-current-time', JSON.stringify(data));
+    console.log('played the video!');
+}
+
+currentTimeFn();
+
+function currentTimeFn() {
+  const array = JSON.parse(localStorage.getItem('videoplayer-current-time'));
+  if (array) {
+    const currentTime = array.seconds;
+    player.setCurrentTime(currentTime);
+  }
+}
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,32 @@
+import throttle from "lodash.throttle";
 
+const form = document.querySelector('form');
+const email = document.querySelector('input');
+const message = document.querySelector('textarea[name = "message"]');
+
+form.addEventListener('input', throttle(onInputForm, 500));
+form.addEventListener('submit', onSubmitForm);
+
+const dataStorage = {};
+
+function onInputForm(e) {
+  dataStorage.email = form.elements.email.value;
+  dataStorage.message = form.elements.message.value;
+  localStorage.setItem('feedback-form-state', JSON.stringify(dataStorage));
+}
+
+function onSubmitForm(e) {
+  e.preventDefault();
+  e.currentTarget.reset();
+  localStorage.removeItem('feedback-form-state');
+  console.log(dataStorage);
+}
+
+storageStatus()
+function storageStatus() {
+    const saveData = JSON.parse(localStorage.getItem('feedback-form-state'));
+    if (saveData) {
+        email.value = saveData.email;
+        message.value = saveData.message;
+    }
+}
